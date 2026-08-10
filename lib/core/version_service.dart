f6cc83fa9ad3b94e5f7643ab8ddcd7e12a26cad8
Sabir Dzhabrailov <SabirDzhabrailov.111@gmail.com>
2026-08-10 15:36:02 +0300

    chore: bump version to 1.1.3

--- lib/core/version_service.dart
+++ lib/core/version_service.dart
@@ -18,7 +18,7 @@ class VersionService {
   // The build pipeline can provide the version with --dart-define=APP_VERSION.
   static const String currentVersion = String.fromEnvironment(
     'APP_VERSION',
-    defaultValue: '1.1.2',
+    defaultValue: '1.1.3',
   );
 
   static final UpdateChecker _defaultChecker = UpdateChecker(
